7319d7d026aaf72e58a26aa9f95f45b102f32440
Nick <nickkoproski@gmail.com>
2024-07-17 16:36:07 -0400

    First commit.

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  root 'homepage#index'
+
+  # Add routes below this line
+  post 'tasks' => 'tasks#create'
+  get 'tasks' => 'tasks#index'
+  delete 'tasks/:id' => 'tasks#destroy'
+  put '/tasks/:id/mark_complete' => 'tasks#mark_complete'
+  put '/tasks/:id/mark_active'   => 'tasks#mark_active'
+  # Add routes below above line
+
+  # Redirect all other paths to index page, which will be taken over by AngularJS
+  get '*path' => 'homepage#index'
+end
